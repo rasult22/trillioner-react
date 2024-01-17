489160925d2d2475c3b5e0d679c1e743380ea5a9
Itachi-MacOS <warrior.006@inbox.ru>
2024-01-18 03:26:08 +0600

    delete tests

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-react-shadcn',
+  appId: 'trillioner.diary.app',
+  appName: 'Ежедневник',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), legacy()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/setupTests.ts",
-	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
